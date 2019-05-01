5230fdd1eaf6fcc57363b0446bdb5b70052eb99e
Drol825 <drol825@gmail.com>
2019-05-01 21:04:46 +0300

    the end

---
+++ index.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="images/icon.png" type="image/png">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Habit tracker</title>
+    <link rel="stylesheet" href="main.css"/>
+    <link href="https://fonts.googleapis.com/css?family=Wendy+One" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Karla" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+</head>
+<body>
+    
+
+    <header>
+        <img src="images/icon.png" alt=""/>
+        <ul>
+            <li id="li_home">Home</li>
+            <li id="li_about">About</li>
+            <li id="li_tracker">Tracker</li>
+            <li id="li_contact">Contact</li>
+        </ul>
+        <a href="login.php"><i class="fas fa-user-alt"></i></a>
+    </header>
+
+    <div class="main">
+        <h1>Your Habits <br>Define Your Life!</h1>
+        <!-- <img id="img_1" src="images/img1.jpg" alt=""/> -->
+        <img id="img_3" src="images/img3.jpeg" alt=""/>
+        <img id="img_4" src="images/img4.jpg" alt=""/>
+        <img id="img_2" src="images/img2.jpg" alt=""/>
+
+        <div class="scroll-downs">
+            <div class="mousey">
+                <div class="scroller"></div>
+            </div>
+        </div>
+    </div>
+
+    <div class="about">
+        <h2>The Habit Tracker: What It Is and How It Works</h2>
+        <div id="content">
+            <p>
+                A habit tracker is a simple way to measure whether you did a habit.
+                The most basic format is to get a calendar and cross off each day you stick with your routine. 
+                For example, if you meditate on Monday, Wednesday, and Friday, each of those dates gets an X. 
+                As time rolls by, the calendar becomes a record of your habit streak.
+                To make this process as easy as possible, We created out virtual habit tracker. 
+                All you have to do is add your habit and start crossing off the days.<br>
+                Habit tracking is powerful for three reasons:
+                <ul>
+                    <li>It creates a visual cue that can remind you to act.</li>  
+                    <li>It is motivating to see the progress you are making. You don't want to break your streak.</li>   
+                    <li>It feels satisfying to record your success in the moment.</li>           
+                </ul>
+                Let's break down each one.
+            </p>
+        </div>
+        <div id="about_img">
+            <img src="images/icon.png" alt=""/>
+        </div>
+    </div>
+
+    <div class="tracker">
+        <p>Are you ready to educate a new habit?</p>
+        <div class="contact-fp-wrap">
+            <svg height="60" width="320" xmlns="http://www.w3.org/2000/svg">
+              <defs>
+                <linearGradient id="logo-gradient" x1="50%" y1="0%" x2="75%" y2="100%" >  
+                    <stop offset="0%" stop-color="#F79533">
+                        <animate attributeName="stop-color" values="#F79533; #F37055; #EF4E7B; #A166AB; #5073B8; #1098AD; #07B39B; #6DBA82; #F79533" dur="4s" repeatCount="indefinite"></animate>
+                    </stop>
+                    <stop offset="100%" stop-color="#F79533">
+                        <animate attributeName="stop-color" values="#F37055; #EF4E7B; #A166AB; #5073B8; #1098AD; #07B39B; #6DBA82; #F79533; #F37055" dur="4s" repeatCount="indefinite"></animate>
+                    </stop>
+                </linearGradient> 
+            </defs>
+            <rect class="booton" height="60" width="320" />
+            </svg>
+            <div class="booton-text"><a href="login_signup.html" style="color:rgba(17, 21, 22, 0.884)">Let's get started!</a></div>
+        </div>
+
+        <img id="tracker--img--1" src="images/watch.png" alt=""/>
+        <img id="tracker--img--2" src="images/apple.png" alt=""/>
+        <img id="tracker--img--3" src="images/settings.png" alt=""/>
+        <img id="tracker--img--4" src="images/man.png" alt=""/>
+    </div>
+
+    <footer>
+        <img src="images/icon.png" alt=""/>
+        <ul id="first--list">
+            Get started
+            <li><a>Home</a></li>
+            <li><a>Sign up</a></li>
+            <li><a>Download</a></li>
+        </ul>
+        <ul id="second--list">
+            About us
+            <li><a>Information</a></li>
+            <li><a>Contact us</a></li>
+        </ul>
+        <ul id="third--list">
+            Support
+            <li><a>Help</a></li>
+        </ul>
+        <div id="icons">
+                <a href="" target="_blank"><span><i title="Facebook" class="fab fa-facebook-f"></i></span></a>
+                <a href="" target="_blank"><span><i title="Instagram" class="fab fa-instagram"></i></span></a>
+                <a href=""><span><i title="Google plus" class="fab fa-google-plus-g"></i></span></a>
+        </div>
+        <button>Contact us</button>
+        <div id="copyright--block"> Copyright © Habit trackers 2019  All rights reserved</div>
+    </footer>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="mainJS.js"></script>
+</body>
+</html>
---
+++ profil.php
@@ -0,0 +1,138 @@
+<?php
+    include('dbcon.php');
+    
+ini_set('display_errors', 1);
+ini_set('error_reporting', E_ALL);
+if (isset($_COOKIE['id']) and isset($_COOKIE['hash']))
+{
+    //$query = mysqli_query($db, "SELECT *,INET_NTOA(user_ip) AS user_ip FROM users WHERE user_id = '".intval($_COOKIE['id'])."' LIMIT 1");
+   // $userdata = mysqli_fetch_assoc($query);
+$query =  $DB->query("SELECT id FROM users WHERE id =?", array($_COOKIE['id']));
+    if($query[0]['id'] !== $_COOKIE['id'])
+    {
+        setcookie("id", "", time() - 3600*24*30*12, "/");
+        setcookie("hash", "", time() - 3600*24*30*12, "/");
+        print "Хм, что-то не получилось";
+    }
+    else
+    {
+$user_ids = $query[0]['id'];
+      if(isset($_POST['name']) AND isset($_POST['from']) AND isset($_POST['to'])){
+      $name = $_POST['name'];
+      $from = $_POST['from'];
+      $to = $_POST['to'];
+              $pieces1 = explode("/", $from);
+              $pieces2 = explode("/", $to);
+
+              $from = $pieces1[1].'-'.$pieces1[0].'-'.$pieces1[2];
+
+              $to = $pieces2[1].'-'.$pieces2[0].'-'.$pieces2[2];
+
+      $DB->query("INSERT INTO `track` (`id`, `user_id`, `name`, `fromT`, `toT`) VALUES (NULL, ?, ?, ?, ?)", array($user_ids,$name,$from,$to));
+
+
+      }
+
+
+$track =  $DB->query("SELECT * FROM `track` WHERE user_id =?", array($user_ids));
+
+
+
+    ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="tracker.css"/>
+    <link rel="stylesheet" href="datedropper.css"/>
+    <link rel="stylesheet" href="my-style.css"/>
+    <link href="https://fonts.googleapis.com/css?family=Wendy+One" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
+</head>
+<body>
+    
+    <header>
+        <div>
+            <h2>Choose a habit and the length of time during which you want to develop this habit.<span></span></h2>
+            <div id="inputs--block">
+              <form method="POST">
+                <input name="name" id="input--name" type="text" placeholder="Habit's name"/>
+                <input name="from" id="input--from" type="text" data-large-mode="true" data-lock="from" data-translate-mode="true"/>
+                <input name="to" id="input--to" type="text" data-large-mode="true" data-lock="from" data-translate-mode="true"/>
+                <button> <input value="submit" type="submit"></button>
+              </form>
+            </div>
+        </div>
+    </header>
+    <section>
+        <table>
+            <tr>
+                <th>Habit's name</th>
+                <th>From</th>
+                <th>To</th>
+                <th>Days</th>
+                <th>Status</th>
+            </tr>
+            <?php
+            foreach ($track as $key => $value) {
+             // print_r($value);
+            $froms = $value['fromT'];
+            $tos = $value['toT'];
+            
+            $datetime1 = strtotime($froms);
+            $datetime2 = strtotime($tos);
+
+                $secs = $datetime2 - $datetime1;// == <seconds between the two times>
+                $days = floor($secs / (24 * 60 * 60 )); ;
+            ?>
+            <tr>
+                <th><?php echo $value['name'] ?></th>
+                <th><?php echo $value['fromT'] ?></th>
+                <th><?php echo $value['toT'] ?></th>
+                <th><?php echo $days ?></th>
+                <th><a name="fb_share"></a></th>
+            </tr>
+            <?php
+          }
+          ?>
+        </table>
+    </section>
+
+    <footer>
+            <img src="images/icon.png" alt=""/>
+            <ul id="first--list">
+                Get started
+                <li><a>Home</a></li>
+                <li><a>Sign up</a></li>
+                <li><a>Download</a></li>
+            </ul>
+            <ul id="second--list">
+                About us
+                <li><a>Information</a></li>
+                <li><a>Contact us</a></li>
+            </ul>
+            <ul id="third--list">
+                Support
+                <li><a>Help</a></li>
+            </ul>
+            <div id="icons">
+                    <a href="" target="_blank"><span><i title="Facebook" class="fab fa-facebook-f"></i></span></a>
+                    <a href="" target="_blank"><span><i title="Instagram" class="fab fa-instagram"></i></span></a>
+                    <a href=""><span><i title="Google plus" class="fab fa-google-plus-g"></i></span></a>
+            </div>
+            <button>Contact us</button>
+            <div id="copyright--block"> Copyright © Habit trackers 2019  All rights reserved</div>
+    </footer>
+    <script src="https://www.facebook.com/connect.php/js/FB.Share" type="text/javascript"></script> 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="datedropper.js"></script>
+    <script src="tracker.js"></script>
+</body>
+</html>
+<?php
+    }}
+?>
